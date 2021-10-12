07be48c2514864d079f4a8ebca9ee7de2dd7d9f7
Durid_Ming <50489088+DuridMing@users.noreply.github.com>
2021-10-12 20:33:06 +0800

    update cv page

--- static/js/webcv.js
+++ static/js/webcv.js
@@ -1,43 +1,19 @@
-// function for churl 
-function churl(){
-        var url = window.location.href;
-       
-        var search_params = url.searchParams;
-        
-        console.log(url);
-        let params = new URLSearchParams(url.search);
+// video set in html
+function vdset(){
+  var data = document.getElementById('vdform').elements['videonum'].value;
+  var divid;
+  
+  console.log(data)
+   $('#vidcontainer').empty();
+  
+  for (let i =1; i<=data; i++){
+    divid = "video-div-"+i.toString();
+    // div
+    $('#vidcontainer').append('<div class="col text-center div-inline-gray" id="'+divid+'">');
+    // title
+    $('#'+divid).append('<p>'+"video"+i.toString()+'</p>');
+    // img
+    $('#'+divid).append('<img src="' + Flask.url_for('Webcv.video_feed', {"num":i }) + '" id="video'+i.toString()+'">');
 
-        var data = document.getElementById('vdform').elements['videonum'].value
-
-        console.log(data);
-        //Add a third parameter.
-        params.set('num', data);
-        console.log(params.toString());
-        window.history.replaceState('', '', "?"+params.toString())
-        window.location.reload()
-      }
-
-// function for video_feed
-function vd_feed_by_url(){
-      var getUrlParameter = function getUrlParameter(sParam) {
-      var sPageURL = window.location.search.substring(1),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
-
-      for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-
-        if (sParameterName[0] === sParam) {
-          return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-        }
-      }
-      return false;
-    };
-    var camnum = getUrlParameter('num');
-    var ci = 0;
-    for (i = 0; i < camnum; i++) {
-      ci = ci + 1;
-      $('#vidcontainer').append('<img src="' + Flask.url_for('Webcv.video_feed', {"num":ci }) + '">');
-    }
+  }
 }
